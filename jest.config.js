568385d5617495e51ae374f33869282047492b4e
Muniru Adam <madam31@academic.rrc.ca>
2025-09-16 11:53:01 -0500

    add changes to jest.config file

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-	preset: "ts-jest",
-	testEnvironment: "node",
+    preset: "ts-jest",
+    testMatch: ["**/*.test.ts"],
+    testEnvironment: "node",
+    collectCoverageFrom: [
+        "src/**/*.ts",
+        "!src/server.ts",
+        "!src/types/**/*.ts",
+    ],
 };
